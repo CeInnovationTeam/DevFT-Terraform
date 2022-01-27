07ef5c54255a05074f65975a980d7e499bb98146
ChristoPedro <pedroegcj@hotmail.com>
2022-01-27 09:12:15 -0300

    Update DevOps

--- devops-variables.tf
+++ devops-variables.tf
@@ -9,10 +9,6 @@ variable "notification_topic_name" {
   default = "devops-topic"
 }
 
-variable "execute_deployment" {
-  default = false
-}
-
 variable "project_description" {
   default = "DevOps Project for OKE deployment"
 }
@@ -39,45 +35,8 @@ variable "project_logging_config_retention_period_in_days" {
   default = 30
 }
 
-
-variable "deploy_artifact_source_type" {
-  default = "INLINE"
-}
-
-variable "deploy_artifact_type" {
-  default = "KUBERNETES_MANIFEST"
-}
-
-variable "argument_substitution_mode" {
-  default = "NONE"
-}
-
 variable "create_dynamic_group_for_devops_pipln_in_compartment" {
   default = true
-}
-
-variable "deploy_pipeline_display_name" {
-  default = "devops-oke-pipeline"
-}
-
-variable "deploy_pipeline_description" {
-  default = "Devops Pipleline demo for OKE"
-}
-
-variable "deploy_stage_deploy_stage_type" {
-  default = "OKE_DEPLOYMENT"
-}
-
-variable "deploy_stage_namespace" {
-  default = "default"
-}
-
-variable "deploy_stage_display_name" {
-  default = "deploy_OKE"
-}
-
-variable "deploy_stage_description" {
-  default = "test deployment to OKE"
 }
 
 variable "deploy_pipeline_deploy_pipeline_parameters_items_default_value" {
--- repo-container.tf
+++
@@ -1,5 +0,0 @@
-resource "oci_artifacts_container_repository" "container_repository" {
-    #Required
-    compartment_id = var.compartment_id
-    display_name = var.container_repository_display_name
-}
